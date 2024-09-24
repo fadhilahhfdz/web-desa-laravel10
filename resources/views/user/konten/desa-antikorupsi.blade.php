3f1cb2f93d6f87608a0c925c1701cc23339fb539
fadill980 <akungithub878@gmail.com>
2024-09-24 13:58:21 +0700

    update 24/09/24

--- resources/views/user/konten/desa-antikorupsi.blade.php
+++ resources/views/user/konten/desa-antikorupsi.blade.php
@@ -2,6 +2,9 @@
 @section('content')
     <div class="container">
         <h2 class="text-center my-3">DESA ANTIKORUPSI</h2>
-        {!! $desaAntikorupsi->first()->konten ?? 'Konten tidak tersedia' !!}
+        {{-- {!! $desaAntikorupsi->first()->konten ?? 'Konten tidak tersedia' !!} --}}
+        @foreach ($desaAntikorupsi as $item)
+            {!! $item->konten !!}
+        @endforeach
     </div>
 @endsection
